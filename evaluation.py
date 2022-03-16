8df0427b0e94351ad173f2c72403b9539e6fff6f
Vargha Khalokhi <vkhalokhi@gmail.com>
2022-03-16 14:29:49 +0330

    evaluation for the model

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,10 @@
 from torch import nn
+import pandas as pd
 import numpy as np
 import torch
 from sklearn import metrics
 from torch import nn
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import transforms
@@ -12,7 +14,7 @@ from models import load_model
 model_path = './best_classification.pth'
 model = load_model("regnet")
 model.load_state_dict(torch.load(model_path))
-model = model.cuda()
+model = model.to("cpu")
 test_transforms = transforms.Compose([
     transforms.Resize((229, 299)),
     transforms.Grayscale(num_output_channels=3),
@@ -57,11 +59,12 @@ def test(test_dataloader, model, criterion, device):
 
     total_loss_valid = loss_ / num_image
     total_acc_valid = valid_acc / num_image
-    print(total_acc_valid, total_loss_valid)
-    return true, pred, image, true_wrong, pred_wrong, total_loss_valid, total_acc_valid.item()
+    print(f"Accuracy: {total_acc_valid.item()}")
+    print(f"Loss: {total_loss_valid}")
+    return true, pred, image, true_wrong, pred_wrong, total_loss_valid, total_acc_valid.item(), model
 
 
-def performance_matrix(true, pred):
+def performance_matrix(true, pred, model):
     precision = metrics.precision_score(true, pred, average='macro')
     recall = metrics.recall_score(true, pred, average='macro')
     accuracy = metrics.accuracy_score(true, pred)
@@ -69,50 +72,12 @@ def performance_matrix(true, pred):
     print('Confusion Matrix:\n', metrics.confusion_matrix(true, pred))
     print('Precision: {} Recall: {}, Accuracy: {}: ,f1_score: {}'.format(precision * 100, recall * 100, accuracy * 100,
                                                                          f1_score * 100))
-
+    #metrics.plot_confusion_matrix(model, true, pred)
 
 if __name__ == '__main__':
-    batch_size = 1
+    batch_size = 2
     criterion = nn.CrossEntropyLoss()
-    true, pred, image, true_wrong, pred_wrong, total_loss_valid, total_acc_valid = test(test_loader, model, criterion,
-                                                                                    "cuda")
-    performance_matrix(true, pred)
+    true, pred, image, true_wrong, pred_wrong, total_loss_valid, total_acc_valid, model = test(test_loader, model, criterion,
+                                                                                    "cpu")
+    performance_matrix(true, pred, model)
 
-# def test(model, dataloader):
-#     running_corrects = 0
-#     running_loss = 0
-#     pred = []
-#     true = []
-#     pred_wrong = []
-#     true_wrong = []
-#     image = []
-#
-#     for batch_idx, (data, target) in enumerate(dataloader):
-#         data, target = Variable(data), Variable(target)
-#         data = data.type(torch.cuda.FloatTensor)
-#         target = target.type(torch.cuda.LongTensor)
-#         model.eval()
-#         output = model(data)
-#         loss = criterion(output, target)
-#         output = sm(output)
-#         _, preds = torch.max(output, 1)
-#         running_corrects = running_corrects + torch.sum(preds == target.data)
-#         running_loss += loss.item() * data.size(0)
-#         preds = preds.cpu().numpy()
-#         target = target.cpu().numpy()
-#         preds = np.reshape(preds, (len(preds), 1))
-#         target = np.reshape(target, (len(preds), 1))
-#         data = data.cpu().numpy()
-#
-#         for i in range(len(preds)):
-#             pred.append(preds[i])
-#             true.append(target[i])
-#             if (preds[i] != target[i]):
-#                 pred_wrong.append(preds[i])
-#                 true_wrong.append(target[i])
-#                 image.append(data[i])
-#
-#     epoch_acc = running_corrects.double() / (len(dataloader) * batch_size)
-#     epoch_loss = running_loss / (len(dataloader) * batch_size)
-#     print(epoch_acc, epoch_loss)
-#     return true, pred, image, true_wrong, pred_wrong
